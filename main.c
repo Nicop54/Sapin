ba564b4dbe138eb461eac039401879e8fed81e6d
Nico <117853100+Nicop54@users.noreply.github.com>
2022-11-15 09:52:51 +0100

    sapin

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int b=0;
-    printf("Hello, World!\n");
+    int l=0,i=0,t=0,e=0;
+    printf("Entrez une largeur :\n");
+    scanf("%d",&l);
+    for(i=1;i<=l ;i++)
+    {
+        for(t=2*l-1;t>=1;t--)
+        {
+            printf(" ");
+            for(i;i=2*l-1;i++){
+                printf("*");
+                printf("\n");
+
+            }
+        }
+
+    }
+
     return 0;
 }
