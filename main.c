7b50af0058e8dfb92400b0767e38662de1338e87
Nico <117853100+Nicop54@users.noreply.github.com>
2022-11-22 18:28:59 +0100

    ca marcheee

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <windows.h>
 
-int main() {
+void Color(int couleurDuTexte,int couleurDeFond) 
+{
+    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H,couleurDeFond*16+couleurDuTexte);
+}
+
+int main(int argc, char *argv[]) {
     int l=0,g=0,i=0,e=0;
-    printf("Entrez une largeur :\n");
+    printf("Entrez une hauteur comprise entre 5 et 20 :\n");
     scanf("%d",&l);
-    for(l;l!=0;l--)
+    if(l>=5 && l<=20)
     {
-        printf(" ");
-        for(i=0;i>=l;(i*2)+1)
+        for(i=0;i<l;i++)
         {
-            printf("*");
+            for(e=l-i;e>0;e--)
+            {
+                printf(" ");
+            }
+            for(g=(i*2)+1;g>0;g--)
+            {
+                Color(2,5);
+                printf("*");
+            }
+
+            printf("\n");
         }
     }
-    printf("\n");
+    else
+    {
+        printf("Erreur, le nombre entré n'est pas compris entre 5 et 20 !");
+    }
     return 0;
 }
