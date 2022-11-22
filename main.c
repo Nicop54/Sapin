538c2d4d69938f74cdf5f454466441b5d770dcb3
Nico <117853100+Nicop54@users.noreply.github.com>
2022-11-22 09:53:52 +0100

    base

--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int x=0,g=0,i=0,e=0;
+    int l=0,g=0,i=0,e=0;
     printf("Entrez une largeur :\n");
-    scanf("%d",&x);
-    for(e=0;i<1;e++)
+    scanf("%d",&l);
+    for(l;l!=0;l--)
     {
-        for(i=x;i>=0;i--)
-        {
-            printf(" ");
-        }
-        for(g=1;g<=x;(g*2)+1)
+        printf(" ");
+        for(i=0;i>=l;(i*2)+1)
         {
             printf("*");
         }
-
-        printf("\n");
     }
+    printf("\n");
     return 0;
 }
-
